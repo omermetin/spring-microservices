99959616452c0b16b3ab7d69f7089b3d6f4846c4
omermetin <omermetin@hotmail.co.uk>
2019-06-18 22:50:35 +0100

    springboot

---
+++ spring-helloworld-cf-om/src/test/java/com/omermetin/SpringHelloworldCfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.omermetin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringHelloworldCfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
